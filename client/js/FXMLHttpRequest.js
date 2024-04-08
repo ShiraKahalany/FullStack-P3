a351d418a28fbc75e6e76dcb8aaf6c358b872970
AvitalHoro <avkahana@g.jct.ac.il>
2024-04-08 23:27:06 +0300

    fxml change

--- client/js/FXMLHttpRequest.js
+++ client/js/FXMLHttpRequest.js
@@ -1,20 +1,69 @@
+//object are used to interact with servers
+
 class FXMLHttpRequest {
+
+    //The constructor initializes an FXMLHttpRequest. It must be called before any other method calls.
     constructor() {
+
+        //Returns a number representing the state of the request.
         this.readyState = 0;
+
+        //Returns the HTTP response status code of the request.
         this.status = 0;
+        //Returns a string containing the response string returned by the HTTP server. 
+        //Unlike XMLHttpRequest.status, this includes the entire text of the response message ("OK", for example).
         this.statusText = '';
+
+        //Returns an ArrayBuffer, a Blob, a Document, a JavaScript object, or a string, 
+        //depending on the value of XMLHttpRequest.responseType, that contains the response entity body.
         this.response = null;
+
+        //Returns a string that contains the response to the request as text, 
+        //or null if the request was unsuccessful or has not yet been sent.
         this.responseText = '';
+
+        //Specifies the type of the response.
         this.responseType = '';
+
+        //Returns the serialized URL of the response or the empty string if the URL is null.
         this.responseURL = '';
+
+        //The time in milliseconds a request can take before automatically being terminated.
         this.timeout = 0;
+
+        //Returns true if cross-site Access-Control requests 
+        //should be made using credentials such as cookies or authorization headers; otherwise false.
         this.withCredentials = false;
+
+        //A XMLHttpRequestUpload representing the upload process.
         this.upload = {};
+
+
         this._listeners = {
+
+            //Fired whenever the readyState property changes.
             readystatechange: [],
+
+            //Fired when an XMLHttpRequest transaction completes successfully. 
             load: [],
+
+            //Fired when a request has completed, 
+            //whether successfully (after load) or unsuccessfully (after abort or error).
+            loadend: [],
+
+            //Fired when a request has started to load data.
+            loadstart: [],
+
+            //Fired periodically when a request receives more data.
+            progress: [],
+
+            //Fired when the request encountered an error.
             error: [],
+
+            //Fired when progress is terminated due to preset time expiring.
             timeout: [],
+
+            //Fired when a request has been aborted, for example because the program called FXMLHttpRequest.abort().
             abort: []
         };
     }
@@ -36,29 +85,29 @@ class FXMLHttpRequest {
         if (this.readyState !== 1) {
             throw new Error('Invalid state');
         }
-        // Implement header setting logic here
+        //header setting logic here
     }
 
     send(data = null) {
         if (this.readyState !== 1) {
             throw new Error('Invalid state');
         }
-        // Implement send logic here
+        //send logic here
         this.dispatchEvent('load');
     }
 
     abort() {
-        // Implement abort logic here
+        //abort logic here
         this.readyState = 0;
         this.dispatchEvent('abort');
     }
 
     getAllResponseHeaders() {
-        // Implement logic to return all response headers
+        // here the logic to return all response headers
     }
 
     getResponseHeader(header) {
-        // Implement logic to return a specific response header
+        // here the logic to return a specific response header
     }
 
     addEventListener(type, listener) {
@@ -86,10 +135,10 @@ class FXMLHttpRequest {
     }
 }
 
-// Example usage
-const request = new FXMLHttpRequest();
-request.addEventListener('load', () => {
-    console.log('Request completed:', request.responseText);
-});
-request.open('GET', 'https://jsonplaceholder.typicode.com/posts/1');
-request.send();
+// // Example usage
+// const request = new FXMLHttpRequest();
+// request.addEventListener('load', () => {
+//     console.log('Request completed:', request.responseText);
+// });
+// request.open('GET', 'https://jsonplaceholder.typicode.com/posts/1');
+// request.send();
