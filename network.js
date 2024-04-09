9f6da7898379185b232245b83ce8e563a1faa105
AvitalHoro <avkahana@g.jct.ac.il>
2024-04-09 14:59:20 +0300

    network

--- network.js
+++ network.js
@@ -1,6 +1,20 @@
 // network.js
 
 export function sendToNetwork(fxml, data) {
-    // Function implementation
+    switch(method)
+    {
+        case 'GET':
+            Server.GET(url, data);
+            break;
+        case 'POST':
+            Server.POST(url, data);
+            break;
+        case 'PUT':
+            Server.PUT(url, data);
+            break;
+        case 'DELETE':
+            Server.DELETE(url, data);
+            break;
+    }
 }
 
