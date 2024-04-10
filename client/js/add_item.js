0c61cceda2035c08aca3285db8ff27dce0fda4c9
AvitalHoro <avkahana@g.jct.ac.il>
2024-04-10 23:50:38 +0300

    add item

--- client/js/add_item.js
+++ client/js/add_item.js
@@ -1,129 +1,135 @@
 import { FXMLHttpRequest } from './FXMLHttpRequest.js';
-
-const images = [
-  '../img/item1.png',
-  '../img/item2.png',
-  '../img/item3.png',
-  '../img/item4.png',
-  '../img/item5.png',
-  '../img/item6.png',
-  '../img/item7.png',
-  '../img/item8.png',
-]
-
-var index = 0;
-var selectedImage;
-
-if (index === 0) {
-  document.getElementById('left-arrow').style.display = 'none';
-}
-
-if (index === images.length - 3) {
-  document.getElementById('right-arrow').style.display = 'none';
-}
-
-document.getElementById('left-arrow').addEventListener('click', () => { index--; goGalleryItems(); });
-document.getElementById('right-arrow').addEventListener('click', () => { index++; goGalleryItems(); });
-
-function goGalleryItems() {
-  if (index !== 0) {
-    document.getElementById('left-arrow').style.display = 'block';
-  }
-  else {
-    document.getElementById('left-arrow').style.display = 'none';
-  }
-
-  if (index === images.length - 3) {
-    document.getElementById('right-arrow').style.display = 'none';
-  }
-  else {
-    document.getElementById('right-arrow').style.display = 'block';
-  }
-  const gallery = document.querySelector('.photo-gallery');
-  gallery.innerHTML = ''; // Clear existing content
-
-  // Create and append new image elements
-  [index, index + 1, index + 2].forEach(index => {
-    const img = document.createElement('img');
-    img.src = images[index];
-    img.alt = `Image ${index + 1}`;
-    img.className = 'gallery-image';
-    img.addEventListener('click', () => {
-      selectedImage = index;
-      document.querySelectorAll('.selected').forEach(img => img.classList.remove('selected'));
-      img.classList.add('selected');
-
-    }  );     
-    gallery.appendChild(img);
-  });
-
-}
-
-goGalleryItems();
-
-
-// const params = new URLSearchParams(window.location.search);
-// const familyData = params.get('family');
-// const family = JSON.parse(decodeURIComponent(familyData));
-
-const family =  {
-    family_id: 1,
-    familyName: "קהלני",
-    password: "123456",
-    familyChildren: ["שי", "רון", "רות", "דניאל", "רועי", "שירה", "אופיר"],
-    startTime: null
-}
-
-function populateResponsibleOptions() {
-const itemResponsibleSelect = document.getElementById('item-responsible');
-
-// Clear existing options
-itemResponsibleSelect.innerHTML = '';
-
-// Create and append options for each child
-family.familyChildren.forEach(child => {
-    const option = document.createElement('option');
-    option.text = child;
-    itemResponsibleSelect.add(option);
-});
-}
-populateResponsibleOptions();
-
-function buildItemObject() {
-  const itemName = document.getElementById('itemName').value;
-  const selectedImageSrc = selectedImage ? selectedImage.src : "../img/default-item-image.png";
-  const responsibleIndex = document.getElementById('item-responsible').selectedIndex;
-  const responsible = family.familyChildren[responsibleIndex];
-  const finishTime = null;
-
-  const newItem = {
-      family_id: family.family_id,
-      itemName: itemName,
-      image: selectedImageSrc,
-      responsible: responsible,
-      finishTime: finishTime
-  };
-
-  console.log("new item:", newItem);
-  return newItem;
-}
-
-document.getElementById('add-item-btn').addEventListener('click', function() {
-  const newItem = buildItemObject();
-  console.log(newItem);
-
-  var request = new FXMLHttpRequest();
-    request.addEventListener('readystatechange', () => {
-        if (request.readyState == 4 && request.status == 200) {
-            alert("נימחקת בהצלחה");
-            window.location.href = "../html/login.html";
-        }
-        else if (request.status===4 && request.status != 200) {
-            alert("שגיאה בהתחברות");
-        }
+window.addEventListener('message', function(event) {
+  console.log("Message received from the parent: " + event.data); // Message received from parent
+  if (event.data === 'render-yourself')
+  {
+    const images = [
+      '../img/item1.png',
+      '../img/item2.png',
+      '../img/item3.png',
+      '../img/item4.png',
+      '../img/item5.png',
+      '../img/item6.png',
+      '../img/item7.png',
+      '../img/item8.png',
+    ]
+    
+    var index = 0;
+    var selectedImage;
+    
+    if (index === 0) {
+      document.getElementById('left-arrow').style.display = 'none';
+    }
+    
+    if (index === images.length - 3) {
+      document.getElementById('right-arrow').style.display = 'none';
+    }
+    
+    document.getElementById('left-arrow').addEventListener('click', () => { index--; goGalleryItems(); });
+    document.getElementById('right-arrow').addEventListener('click', () => { index++; goGalleryItems(); });
+    
+    function goGalleryItems() {
+      if (index !== 0) {
+        document.getElementById('left-arrow').style.display = 'block';
+      }
+      else {
+        document.getElementById('left-arrow').style.display = 'none';
+      }
+    
+      if (index === images.length - 3) {
+        document.getElementById('right-arrow').style.display = 'none';
+      }
+      else {
+        document.getElementById('right-arrow').style.display = 'block';
+      }
+      const gallery = document.querySelector('.photo-gallery');
+      gallery.innerHTML = ''; // Clear existing content
+    
+      // Create and append new image elements
+      [index, index + 1, index + 2].forEach(index => {
+        const img = document.createElement('img');
+        img.src = images[index];
+        img.alt = `Image ${index + 1}`;
+        img.className = 'gallery-image';
+        img.addEventListener('click', () => {
+          selectedImage = index;
+          document.querySelectorAll('.selected').forEach(img => img.classList.remove('selected'));
+          img.classList.add('selected');
+    
+        }  );     
+        gallery.appendChild(img);
+      });
+    
+    }
+    
+    goGalleryItems();
+    
+    
+    // const params = new URLSearchParams(window.location.search);
+    // const familyData = params.get('family');
+    // const family = JSON.parse(decodeURIComponent(familyData));
+    
+    const family =  {
+        family_id: 1,
+        familyName: "קהלני",
+        password: "123456",
+        familyChildren: ["שי", "רון", "רות", "דניאל", "רועי", "שירה", "אופיר"],
+        startTime: null
+    }
+    
+    function populateResponsibleOptions() {
+    const itemResponsibleSelect = document.getElementById('item-responsible');
+    
+    // Clear existing options
+    itemResponsibleSelect.innerHTML = '';
+    
+    // Create and append options for each child
+    family.familyChildren.forEach(child => {
+        const option = document.createElement('option');
+        option.text = child;
+        itemResponsibleSelect.add(option);
     });
-    request.open('POST', 'families', true);
-    request.send(JSON.stringify(newItem));
-  // addItemToClean(newItem);
-  // window.location.href = "./DBud.html?family=" + encodeURIComponent(JSON.stringify(family));
+    }
+    populateResponsibleOptions();
+    
+    function buildItemObject() {
+      const itemName = document.getElementById('itemName').value;
+      const selectedImageSrc = selectedImage ? selectedImage.src : "../img/default-item-image.png";
+      const responsibleIndex = document.getElementById('item-responsible').selectedIndex;
+      const responsible = family.familyChildren[responsibleIndex];
+      const finishTime = null;
+    
+      const newItem = {
+          family_id: family.family_id,
+          itemName: itemName,
+          image: selectedImageSrc,
+          responsible: responsible,
+          finishTime: finishTime
+      };
+    
+      console.log("new item:", newItem);
+      return newItem;
+    }
+    
+    document.getElementById('add-item-btn').addEventListener('click', function() {
+      const newItem = buildItemObject();
+      console.log(newItem);
+    
+      var request = new FXMLHttpRequest();
+        request.addEventListener('readystatechange', () => {
+            if (request.readyState == 4 && request.status == 200) {
+                alert("נימחקת בהצלחה");
+                window.location.href = "../html/login.html";
+            }
+            else if (request.status===4 && request.status != 200) {
+                alert("שגיאה בהתחברות");
+            }
+        });
+        request.open('POST', 'families', true);
+        request.send(JSON.stringify(newItem));
+      // addItemToClean(newItem);
+      // window.location.href = "./DBud.html?family=" + encodeURIComponent(JSON.stringify(family));
+    });
+  }
 });
+
--- client/js/list.js
+++ client/js/list.js
@@ -1,9 +1,39 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const cancelBtns = document.querySelectorAll('.cancel-btn');
+import { FXMLHttpRequest } from './FXMLHttpRequest.js';
 
-    cancelBtns.forEach(btn => {
-      btn.addEventListener('click', () => {
-      console.log('cancel button clicked');
+window.addEventListener('message', function(event) {
+  if (event.data === 'render-yourself')
+  {
+    console.log("Message received from the parent: " + event.data); // Message received from parent
+    const searchInput = document.getElementById('searchInput');
+    const items = document.querySelectorAll('.item');
+    
+    searchInput.addEventListener('input', function() {
+      const searchTerm = this.value.trim();
+      
+      items.forEach(item => {
+        const textContent = item.querySelector('span').textContent.trim();
+        if (textContent.includes(searchTerm)) {
+          item.style.display = 'flex';
+        } else {
+          item.style.display = 'none';
+        }
       });
     });
-  });
+    
+    
+    var fxml = new FXMLHttpRequest();
+    fxml.addEventListener('readystatechange', () => {
+        if (fxml.readyState == 4 && fxml.status == 200) {
+          const Items = JSON.parse(fxml.response);
+          console.log(Items);
+        }
+        else if (fxml.status===4 && fxml.status != 200) {
+            alert("שגיאה בקבלת המידע מהשרת");
+        }
+    });
+    fxml.open('GET', 'itemsToClean', true);
+    fxml.send();
+    
+  }
+});
+
