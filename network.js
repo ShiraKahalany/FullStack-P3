f2383b28e1349dc62bd2e603aa90f614adbbd098
AvitalHoro <avkahana@g.jct.ac.il>
2024-04-11 08:34:49 +0300

    network

--- network.js
+++ network.js
@@ -28,3 +28,20 @@ export function sendToNetwork(fxml, data) { //data=null כשקיבל
     return fxml;
 }
 
+export function SendForServerToNetwork(fxml, sucsess)
+{
+    console.log('SendForServerForNetwork called');
+    if(sucsess)
+    {
+        fxml.status = 200;
+        
+    }
+    else
+    {
+        fxml.status = 404;
+    }
+    fxml.readyState = 4;
+    fxml.dispatchEvent('readystatechange');
+
+}
+
